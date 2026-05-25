bb4fa86400058dbd2db1c37d47780bda56cf2204
vinaawati3-lgtm <vinaawati3@gmail.com>
2026-05-25 12:56:37 +0700

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,73 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleHybrid_0 = new ol.layer.Tile({
+            'title': 'Google Hybrid',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: '<a href="https://www.google.at/permissions/geoguidelines/attr-guide.html">Map data ©2015 Google</a>',
+                url: 'https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}'
+            })
+        });
+var format_ADMINISTRASIKECAMATAN_AR_50K_1 = new ol.format.GeoJSON();
+var features_ADMINISTRASIKECAMATAN_AR_50K_1 = format_ADMINISTRASIKECAMATAN_AR_50K_1.readFeatures(json_ADMINISTRASIKECAMATAN_AR_50K_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_ADMINISTRASIKECAMATAN_AR_50K_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_ADMINISTRASIKECAMATAN_AR_50K_1.addFeatures(features_ADMINISTRASIKECAMATAN_AR_50K_1);
+var lyr_ADMINISTRASIKECAMATAN_AR_50K_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_ADMINISTRASIKECAMATAN_AR_50K_1, 
+                style: style_ADMINISTRASIKECAMATAN_AR_50K_1,
+                popuplayertitle: 'ADMINISTRASIKECAMATAN_AR_50K',
+                interactive: true,
+                title: '<img src="styles/legend/ADMINISTRASIKECAMATAN_AR_50K_1.png" /> ADMINISTRASIKECAMATAN_AR_50K'
+            });
+var format_jalan_2 = new ol.format.GeoJSON();
+var features_jalan_2 = format_jalan_2.readFeatures(json_jalan_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_jalan_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_jalan_2.addFeatures(features_jalan_2);
+var lyr_jalan_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_jalan_2, 
+                style: style_jalan_2,
+                popuplayertitle: 'jalan',
+                interactive: true,
+                title: '<img src="styles/legend/jalan_2.png" /> jalan'
+            });
+var format_titik_3 = new ol.format.GeoJSON();
+var features_titik_3 = format_titik_3.readFeatures(json_titik_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_titik_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_titik_3.addFeatures(features_titik_3);
+var lyr_titik_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_titik_3, 
+                style: style_titik_3,
+                popuplayertitle: 'titik',
+                interactive: true,
+                title: '<img src="styles/legend/titik_3.png" /> titik'
+            });
+
+lyr_GoogleHybrid_0.setVisible(true);lyr_ADMINISTRASIKECAMATAN_AR_50K_1.setVisible(true);lyr_jalan_2.setVisible(true);lyr_titik_3.setVisible(true);
+var layersList = [lyr_GoogleHybrid_0,lyr_ADMINISTRASIKECAMATAN_AR_50K_1,lyr_jalan_2,lyr_titik_3];
+lyr_ADMINISTRASIKECAMATAN_AR_50K_1.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'Shape_Leng': 'Shape_Leng', 'Shape_Area': 'Shape_Area', });
+lyr_jalan_2.set('fieldAliases', {'id': 'id', 'area': 'area', });
+lyr_titik_3.set('fieldAliases', {'id': 'id', 'kecamatan': 'kecamatan', 'kelurahan': 'kelurahan', });
+lyr_ADMINISTRASIKECAMATAN_AR_50K_1.set('fieldImages', {'KDPPUM': 'TextEdit', 'NAMOBJ': 'TextEdit', 'REMARK': 'TextEdit', 'KDPBPS': 'TextEdit', 'FCODE': 'TextEdit', 'LUASWH': 'TextEdit', 'UUPP': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'KDEBPS': 'TextEdit', 'KDEPUM': 'TextEdit', 'KDCBPS': 'TextEdit', 'KDCPUM': 'TextEdit', 'KDBBPS': 'TextEdit', 'KDBPUM': 'TextEdit', 'WADMKD': 'TextEdit', 'WIADKD': 'TextEdit', 'WADMKC': 'TextEdit', 'WIADKC': 'TextEdit', 'WADMKK': 'TextEdit', 'WIADKK': 'TextEdit', 'WADMPR': 'TextEdit', 'WIADPR': 'TextEdit', 'TIPADM': 'TextEdit', 'Shape_Leng': 'TextEdit', 'Shape_Area': 'TextEdit', });
+lyr_jalan_2.set('fieldImages', {'id': '', 'area': '', });
+lyr_titik_3.set('fieldImages', {'id': '', 'kecamatan': '', 'kelurahan': '', });
+lyr_ADMINISTRASIKECAMATAN_AR_50K_1.set('fieldLabels', {'KDPPUM': 'no label', 'NAMOBJ': 'no label', 'REMARK': 'no label', 'KDPBPS': 'no label', 'FCODE': 'no label', 'LUASWH': 'no label', 'UUPP': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDBBPS': 'no label', 'KDBPUM': 'no label', 'WADMKD': 'no label', 'WIADKD': 'no label', 'WADMKC': 'no label', 'WIADKC': 'no label', 'WADMKK': 'no label', 'WIADKK': 'no label', 'WADMPR': 'no label', 'WIADPR': 'no label', 'TIPADM': 'no label', 'Shape_Leng': 'no label', 'Shape_Area': 'no label', });
+lyr_jalan_2.set('fieldLabels', {'id': 'no label', 'area': 'no label', });
+lyr_titik_3.set('fieldLabels', {'id': 'no label', 'kecamatan': 'no label', 'kelurahan': 'no label', });
+lyr_titik_3.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
